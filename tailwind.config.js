7ede738c75777222812ba6b60a39fb9ecadc783a
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-12-05 18:18:40 +0530

    Tried implementing postcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      flex: {
+        '2': '2.5 2 0%',
+      },
+      height: {
+        'mid-screen-1': '68vh',
+        'mid-screen-2': '100vh',
+        'full-screen': '100%',
+        '120': '30rem',
+        '144': '36rem',
+      },
+      width: {
+        '144': '37rem',
+      },
+      fontFamily: { 
+        'dm-sans': ['DM Sans', 'sans-serif' ]
+      },
+      backgroundImage: {
+        "dark-blue":"linear-gradient(252deg, #60d9f7 102%, #58baf6 59%, red 35%, #4d8cf4 8%)"
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            },
+            {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
